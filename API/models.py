8e9bb472d0d7d90a69996f462e21a52317c577fc
AliciaCampo <145765516+AliciaCampo@users.noreply.github.com>
2025-02-12 21:45:08 +0100

    todos los creates en models.py

--- API/models.py
+++ API/models.py
@@ -10,13 +10,21 @@ class Usuari(BaseModel):
     correu: str
     contrasenya: str
     llistes: List[int] = []
-
+class UsuariCreate(BaseModel):
+    nom: str
+    imatge: Optional[str] = None
+    edat: int
+    correu: str
+    contrasenya: str 
 class Llista(BaseModel):
     titol: str
     descripcio: Optional[str] = None
     privada: bool
     titols: List[int] = []
-
+class LlistaCreate(BaseModel):
+    titol: str
+    descripcio: Optional[str] = None
+    privada: bool  # No incluye `titols` al momento de crear
 class Titol(BaseModel):
     imatge: Optional[str] = None
     nom: str
@@ -25,3 +33,10 @@ class Titol(BaseModel):
     es_peli: bool
     rating: float
     comentaris: Optional[str] = None
+class TitolCreate(BaseModel):
+    imatge: Optional[str] = None
+    nom: str
+    descripcio: Optional[str] = None
+    plataformes: str
+    es_peli: bool
+    rating: float
